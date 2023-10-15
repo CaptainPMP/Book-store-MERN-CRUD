9a220c2040610fe452993bedb30168beb2071520
CaptainPMP <scikung8@gmail.com>
2023-10-15 17:15:56 +0700

    Start MERN CRUD

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://root:root@books-store-mern.jfjbzhk.mongodb.net/books-collection?retryWrites=true&w=majority'
+// Please create a free dataabse for yourself
+// This database will be deleted after the tutorial
---
+++ backend/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
+import { Book } from './models/bookModel.js';
+import booksRoutes from './routes/booksRoute.js'
+import cors from 'cors'
+
+const app = express();
+
+app.use(express.json())
+
+// Middleware for handling CORS POLICY
+app.use(cors());
+
+app.use(
+    cors({
+        origin: 'https://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+)
+
+app.get("/", (req, res) => {
+    res.status(200).send('Welcome')
+})
+
+app.use('/books', booksRoutes)
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log("App connected to database");
+
+        app.listen(PORT, () => {
+            console.log(`listening on port ${PORT}`);
+        })
+    })
+    .catch(err => {
+        console.error(err);
+    })
