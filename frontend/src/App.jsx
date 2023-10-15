f57ed90495975a973a58b96f28f40fb5d2e14c85
CaptainPMP <scikung8@gmail.com>
2023-10-15 19:44:42 +0700

    fix bug because <Route ... element's'> <-- this 's' here is the error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
-import {Routes, Route} from 'react-router-dom'
-
-import Home from './pages/Home'
-import CreateBooks from './pages/CreateBooks'
-import ShowBook from './pages/ShowBook'
-import EditBook from './pages/EditBook'
-import DeleteBook from './pages/DeleteBook'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CreateBook from './pages/CreateBooks';
+import ShowBook from './pages/ShowBook';
+import EditBook from './pages/EditBook';
+import DeleteBook from './pages/DeleteBook';
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" elements={<Home />} />
-      <Route path="/books/create" elements={<CreateBooks />} />
-      <Route path="/books/details/:id" elements={<ShowBook />} />
-      <Route path="/books/edit/:id" elements={<EditBook />} />
-      <Route path="/books/delete/:id" elements={<DeleteBook />} />
+      <Route path='/' element={<Home />} />
+      <Route path='/books/create' element={<CreateBook />} />
+      <Route path='/books/details/:id' element={<ShowBook />} />
+      <Route path='/books/edit/:id' element={<EditBook />} />
+      <Route path='/books/delete/:id' element={<DeleteBook />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,10 @@
-import React, {useEffect, useState} from 'react'
-import axios from 'axios'
-import Spinner from '../components/Spinner'
-import {Link} from 'react-router-dom'
-import {AiOutlineEdit} from 'react-icons/ai'
-import {BsInfoCircle} from 'react-icons/bs'
-import {MdOutlineAddbox, MdOutlineDelete} from 'react-icons/md'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Spinner from '../components/Spinner';
+import { Link } from 'react-router-dom';
+import { AiOutlineEdit } from 'react-icons/ai';
+import { BsInfoCircle } from 'react-icons/bs';
+import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
 
 const Home = () => {
   const [books, setBooks] = useState([])
@@ -21,13 +21,13 @@ const Home = () => {
         console.error(err)
         setLoading(false)
       })
-  })
+  }, [])
   return (
     <div className='p-4'>
       <div className="flex justify-between items-center">
         <h1 className="text-3xl my-8">Books List</h1>
         <Link to="/books/create">
-          <MdOutlineAddbox className="text-sky-800 text-4xl" />
+          <MdOutlineAddBox className="text-sky-800 text-4xl" />
         </Link>
       </div>
 
