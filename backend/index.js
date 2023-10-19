55bbd7abf70d569858d93dd1f7de065f40bd1d98
CaptainPMP <scikung8@gmail.com>
2023-10-19 21:08:44 +0700

    update latest to 19/10/2023

--- backend/index.js
+++ backend/index.js
@@ -12,13 +12,13 @@ app.use(express.json())
 // Middleware for handling CORS POLICY
 app.use(cors());
 
-app.use(
-    cors({
-        origin: 'https://localhost:3000',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type'],
-    })
-)
+// app.use(
+//     cors({
+//         origin: 'https://localhost:3000',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type'],
+//     })
+// )
 
 app.get("/", (req, res) => {
     res.status(200).send('Welcome')
